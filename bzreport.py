039b88d292262d91c3a74b23c2250bad67f2c463
Olaf Heinemann <olaf.heinemann@me.com>
2017-08-30 21:35:22 +0200

    Validierung eines übergebenen Datums
    Mittels —datum oder -d ein Datum übergeben werden, ab dem exportierte AppleHealth-Datensätze verarbeitet werden sollen.
    Erlaubt sind als Datumsformat ISO8601:2004 und DIN 5008 mit 4stelliger Jahreszahl.
    Neben der formalen Korrektheit wird zusätzlich geprüft, ob das Datum auch valide ist.
    Wird kein Datum gesetzt wird der 12.10.2015 als Starttermin verwendet. Dies kann durch Anpassung der Variablen
    - abJahr
    - abMonat
    - abTag
    angepasst werden.
    
    Signed-off-by: Olaf Heinemann <olaf.heinemann@me.com>

--- bzreport.py
+++ bzreport.py
@@ -6,18 +6,165 @@ import datetime as zeit
 import locale
 import os
 import platform
+import re as regex
 import sys
 import time
 
-quelldatei = ''
-zieldatei = ''
+quelldatei = None
+zieldatei = None
+
+# Meine persönlichen Messdaten werden seit 12.10.2015 aufgezeichnet.
+# Die folgenden Datumsangaben müssen also individuell angepasst werden.
+abJahr = 2015
+abMonat = 10
+abTag = 12
+
 # 'abDatum' ist ist nur ein Platzhalter und **muss** entweder durch den mittels '--datum' übergebenen
 # Wert oder durch das Datum des ersten Datensatzes aus der Quelldatei geändert werden!
-abDatum = '1970-01-01'
+abDatum = zeit.date(abJahr, abMonat, abTag).isoformat()
 
 # 'bisDatum' ist ebenfalls nur ein Platzhalter und **muss** durch das Datum des
 # letzten Datensatzes aus der Quelldatei geändert werden!
 bisDatum = zeit.date.today().isoformat()
+
+
+# Meine Funktionen
+
+def datum_entspricht_iso8601(eingegebenes_datum=abDatum):
+    """
+    Prüft, ob das übergebene Datum der ISO 8601:2004 entspricht.
+    Zeitpunkte vor 2000-01-01 werden ignoriert!
+    Siehe auch https://de.wikipedia.org/wiki/Datumsformat#ISO_8601_und_EN_28601
+
+    :param eingegebenes_datum: String
+    :return: Boolian
+    """
+    iso8601_regex = r"(20[0-9]{2})(-?)(1[0-2]|0[1-9])\2(3[01]|0[1-9]|[12][0-9])\Z"
+    iso8601_muster = regex.compile(iso8601_regex)
+
+    if iso8601_muster.fullmatch(eingegebenes_datum):
+        return True
+    else:
+        return False
+
+
+def datum_entspricht_din5008(eingegebenes_datum=abDatum):
+    """
+    Prüft ob das übergebene Datum der DIN 5008 entspricht.
+    Zeitpunkte vor 2000-01-01 werden ignoriert!
+    Siehe auch https://de.wikipedia.org/wiki/Datumsformat#DIN_5008.
+
+    :param eingegebenes_datum: String
+    :return: Boolian
+    """
+    din5008_regex = r"(3[0-1]|2[0-9]|1[0-9]|0[1-9])\.(1[0-2]|0[1-9])\.20([0-9]{2})\Z"
+    din5008_muster = regex.compile(din5008_regex)
+
+    if din5008_muster.fullmatch(eingegebenes_datum):
+        return True
+    else:
+        return False
+
+
+def ist_datum_valide(eingegebenes_datum=abDatum):
+    """
+    Prüft, ob das übergebene Datum auch gültig ist.
+    Dabei wird zwischen den Datumsformaten ISO 8601:2004 und DIN 5008 unterschieden.
+    Wird kein Datumsformat übergeben, wird das Format ISO 8601:2004 verwendet.
+
+    :param eingegebenes_datum: String
+    :return: Boolian
+    """
+
+    datumsformat = None
+    tag = None
+    monat = None
+    jahr = None
+
+    if datum_entspricht_iso8601(eingegebenes_datum):
+        datumsformat = 'iso8601'
+
+    if datum_entspricht_din5008(eingegebenes_datum):
+        datumsformat = 'din5008'
+
+    if datumsformat == 'iso8601':
+        liste = eingegebenes_datum.split('-')
+
+        for _ in liste:
+            tag = int(liste.pop())
+            monat = int(liste.pop())
+            jahr = int(liste.pop())
+
+
+    elif datumsformat == 'din5008':
+        liste = eingegebenes_datum.split('.')
+
+        for _ in liste:
+            jahr = int(liste.pop())
+            monat = int(liste.pop())
+            tag = int(liste.pop())
+    else:
+        tag = 0
+        monat = 0
+        jahr = 0
+
+    # Prüfen, ob Schaltjahr oder nicht
+    tage_im_monat = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+
+    if monat == 2 and jahr % 4 == 0 and (jahr % 100 != 0 or jahr % 400 == 0):
+        if 1 <= tag <= 29:
+            return True
+        else:
+            return False
+    elif monat >= 1 and tag <= tage_im_monat[monat - 1]:
+        return True
+    else:
+        return False
+
+
+def ab_datum_nach_applehealth_datum(eingegebenes_datum=abDatum):
+    """
+    Konvertiert das übergebene Datum in das AppleHealth-Format '%d-%b-%Y'.
+
+    :param eingegebenes_datum: String
+    :return: iso_datum.strftime('%d-%b-%Y')
+    """
+
+    tag = None
+    monat = None
+    jahr = None
+    ah_datum = None
+    datumsformat = None
+
+    if datum_entspricht_iso8601(eingegebenes_datum):
+        datumsformat = 'iso8601'
+
+    if datum_entspricht_din5008(eingegebenes_datum):
+        datumsformat = 'din5008'
+
+    if datumsformat == 'iso8601':
+        liste = eingegebenes_datum.split('-')
+
+        for _ in liste:
+            tag = int(liste.pop())
+            monat = int(liste.pop())
+            jahr = int(liste.pop())
+
+    elif datumsformat == 'din5008':
+        liste = eingegebenes_datum.split('.')
+
+        for _ in liste:
+            jahr = int(liste.pop())
+            monat = int(liste.pop())
+            tag = int(liste.pop())
+
+    else:
+        print("Fehler: kein ah_datum")
+        return ah_datum
+
+    ah_datum = zeit.datetime(jahr, monat, tag).strftime('%d-%b-%Y')
+    return ah_datum
+
 
 # 1. Aufruf und übergebene Parameter prüfen
 parser = argparse.ArgumentParser(
@@ -47,35 +194,34 @@ if argumente.zieldatei:
     print("Ziel-Datei: {0}".format(zieldatei))
 
 if argumente.datum:
-    # @TODO: Prüfen, ob das darauf folgende Argument ein String ist, der einem der folgenden Muster entspricht:
-    # @TODO:     - YYYY-MM-DD
-    # @TODO:     - D[D].M[M].YY[YY]
-    # @TODO: Wenn ja, handelt es sich um ein gültiges Datum?
-    # @TODO: Wenn nicht, dann das Programm mit einer entsprechenden Fehlermeldung abbrechen.
-    print("Es werden nur Datensätze ab dem Datum {0} verarbeitet.".format(argumente.datum))
+    neues_abDatum = argumente.datum
 
+    if ist_datum_valide(neues_abDatum):
+        print("\033[0;33mEs werden nur Datensätze ab dem Datum {0} verarbeitet.\033[0m".format(argumente.datum))
+        abDatum = neues_abDatum
+    else:
+        print("\n\033[0;31mDas übergebene Datum {0} ist ungültig!\033[0m".format(argumente.datum))
+        print("Das Datum muss der ISO 8601:2004 oder der DIN 5008 (4stellige Jahreszahl) entsprechen.")
+        print("\033[0;33mHINWEIS:\033[0m Ein Datum vor dem 01.01.2000 wird ignoriert!")
+        print("\n\033[1;37mBeispiele:\033[1;32m")
+        print("    - 01.01.2000")
+        print("    - 2000-12-31")
+        print("    - 2001-01-01")
+        print("    - 11.11.2001")
+        print("\033[0m")
+        exit(1)
 
 # 3. Eingabe-Datei als CSV-Datei öffnen/einlesen.
 print("\n\nDatei einlesen …")
 csvQuelle = open(quelldatei)
 csvQuelldatenLeser = csv.reader(csvQuelle)
 
-
-# 4. Übergebenes Datum verarbeiten.
-# @TODO: Wenn ein Datum übergeben wurde, dann müssen wir nach diesem Tag suchen.
-# @TODO: Dazu müssen wir das angegebene Datum in das Format
-# @TODO:    'DD-[Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec]-YYYY' umwandeln.
-# @TODO: Wenn es keinen Datensatz zu diesem Tag gibt, dann wird der nächstfolgende Tag als neuer Startpunkt verwendet.
-# @TODO: Und zwar solange, bis das Dateiende der Eingabe-Datei erreicht ist.
-# @TODO: Finden wir keinen Startpunkt, dann brechen wir das Programm mit einer Fehlermeldung ab.
-
-
-# 5. Ziel-Datei öffnen
+# 4. Ziel-Datei öffnen
 print("\nDaten schreiben …")
 csvZiel = open(zieldatei, 'w')
 
-# 6. Kopfzeilen schreiben
-print('Blutzuckerwerte vom {0} bis zum {1}'.format(abDatum, bisDatum))
+# 5. Kopfzeilen schreiben
+print('Blutzuckerwerte von {0} bis {1}'.format(abDatum, bisDatum))
 csvFeldnamen = ['Datum', 'Uhrzeit', 'Blutzuckerwert (md/dL)']
 csvZieldatenSchreiber = csv.DictWriter(csvZiel, fieldnames=csvFeldnamen)
 csvZieldatenSchreiber.writeheader()
@@ -85,6 +231,16 @@ csvZieldatenSchreiber.writeheader()
 
 for zeile in csvQuelldatenLeser:
     # 8. Wurde ein Datum übergeben, müssen wir prüfen, ob der eingelesen Datensatz dazu passt.
+    # @TODO: Dazu müssen wir das angegebene Datum in das Format
+    # @TODO:    'DD-[Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec]-YYYY' umwandeln.
+
+    applehealthDatum = ab_datum_nach_applehealth_datum(abDatum)
+
+
+    # @TODO: Wenn es zu diesem Tag keinen Datensatz gibt, wird das nächstfolgende Datum als neuer Startpunkt verwendet.
+    # @TODO: Und zwar solange, bis das Dateiende der Eingabe-Datei erreicht ist.
+    # @TODO: Finden wir keinen Startpunkt, dann brechen wir das Programm mit einer Fehlermeldung ab.
+    #
     # @TODO: Haben wir einen Datensatz gefunden, dann
     # @TODO:    - holen wir uns den Inhalt der ersten und dritten Spalte und übergeben ihn an die Variablen
     # @TODO:        - importZeitstempel und wertBlutzucker.
@@ -107,9 +263,7 @@ csvZiel.close()
 csvQuelle.close()
 
 # 12. Rückmeldung an den Benutzer geben.
+print("\033[1;32m")
 print("Konvertierung abgeschlossen.")
 print("Die Datei {0} kann nun an den Diabetologen gesendet werden.".format(zieldatei))
-
-
-
-
+print("\033[0m")
